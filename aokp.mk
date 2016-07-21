2ed0f81dd3c6c4d6255c11eb6c47067d58678db8
qubex22 <joroga2212@gmail.com>
2016-07-21 04:54:25 +0200

    fresh start from zf5 mm sources (probably need a lot of fixes)

---
+++ aokp.mk
@@ -0,0 +1,37 @@
+#
+# Copyright 2016 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Boot animation
+TARGET_SCREEN_WIDTH  := 1080
+TARGET_SCREEN_HEIGHT := 1920
+
+# Inherit some common CM stuff.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product-if-exists, vendor/aokp/configs/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/lenovo/redhookbay/device.mk)
+
+DEVICE_PACKAGE_OVERLAYS += device/lenovo/redhookbay/overlay
+
+PRODUCT_RUNTIMES := runtime_libart_default
+
+# Device identifier. This must come after all inclusions
+PRODUCT_NAME := aokp_redhookbay
+PRODUCT_BRAND := LENOVO
+PRODUCT_MODEL := LENOVO_K900
+PRODUCT_MANUFACTURER := LENOVO
+PRODUCT_DEVICE := K900
